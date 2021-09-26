57b5388604de48339372fdda42bbbdb43b2c21f7
amishranpariya <amishranpariya@gmail.com>
2021-09-26 10:08:16 +0530

    refactored

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # sine-wave-pipe-animation
-Animation created with circles and sine function with p5.js. I am accepting pull requests for bug fixes, minor improvements and visual design.
 
-* [See the Animation!!](https://amishranpariya.github.io/sine-wave-pipe-animation/)
+Animation created with circles and sine function with "p5.js". I am accepting pull requests for bug fixes and minor improvements.
+
+- [See the Animation!!](https://amishranpariya.github.io/sine-wave-pipe-animation/)
+
+Screenshot:
+![screenshot](screenshot.jpg)
--- sketch.js
+++ sketch.js
@@ -1,36 +1,27 @@
-var spc = 3,
-  r, j = 0;
-var shift=90.5;
+var j = 0;
 
 function setup() {
-createCanvas(windowWidth, windowWidth);
-  //  createCanvas(400,400);
+	createCanvas(400, 400);
+	angleMode(DEGREES);
+	stroke(20);
+	strokeWeight(1);
+	fill(255, 0, 200, 130);
 }
 
 function draw() {
-  background(200);
- // j=map(mouseX,0,width,0,0.5);
-//  spc=map(mouseY,0,height,1,10);
-  wavedraw(height/18,width/6*1, height, 180,j);
-  wavedraw(height/18,width/6*2,height,shift,j);
-  wavedraw(height/18,width/6*3,height,180,j);
-  wavedraw(height/18,width/6*4,height,shift,j);
-  wavedraw(height/18,width/6*5,height,180,j);
-  j-=0.2;
-  
+	background(200);
+	wavedraw(40, 65 * 1, 360, 0);
+	wavedraw(40, 65 * 2, 360, 90);
+	wavedraw(40, 65 * 3, 360, 0);
+	wavedraw(40, 65 * 4, 360, 90);
+	wavedraw(40, 65 * 5, 360, 0);
+	j -= 3;
 }
 
-function wavedraw(strt,strtx, endof, phs, stp = 0.05) {
-  stroke(0);
-  strokeWeight(3);
-  fill(255, 130);
- // var j;
-  for (var i = strt; i < endof / 3-height/18; i += spc) {
-    r = cos(i*2 + j+phs*2) * width/18+width/9-width/24;
+function wavedraw(startY, startX, endY, phase) {
+	for (var i = startY; i < endY; i += 8) {
+		let r = (cos(i * 1.3 + j + phase) * width) / 10;
 
-    ellipse(strtx, i * 3, r, r * 0.6);
-
-
-  }
-  //j += stp;
+		ellipse(startX, i, r, r * 0.6);
+	}
 }
